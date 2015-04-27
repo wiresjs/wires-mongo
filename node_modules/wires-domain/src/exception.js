95177ee46fd3a63798875967ddd03123ff88e0ec
Ivan.Orlov <ivan@morrr.com>
2015-04-27 10:41:31 +0300

    Git ignore added

--- node_modules/wires-domain/src/exception.js
+++
@@ -1,8 +0,0 @@
-var Class = require('wires-class');
-module.exports = Class.extend({
-	initialize : function(message, code)
-	{
-		this.code = code || 500;
-		this.message = message || 'Something went wrong';
-	}
-});
--- node_modules/wires-domain/src/factory.js
+++
@@ -1,8 +0,0 @@
-var domain = require('../index');
-var Class = require('wires-class');
-
-module.exports = Class.extend({
-	init: function() {},
-}, {
-	__domain_factory__: true
-});
